4de81c9e8dc081f02b5c13189f2333f99d3c8283
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2023-10-30 00:16:34 +0800

    fix: css styling

--- src/views/createRecipe.vue
+++ src/views/createRecipe.vue
@@ -6,101 +6,102 @@
 
         <div class="row">
           <!-- Column for Photo Upload -->
-          <div class="col-md-6">
-            <div class="form-group mt-4">
-              <label for="imageUpload" class="form-label">Recipe Image:</label>
-              <input type="file" id="imageUpload" @change="onImageSelected" class="form-control">
+
+              <div class="form-group mt-4">
+                <label for="imageUpload" class="form-label">Recipe Image:</label>
+                <input type="file" id="imageUpload" @change="onImageSelected" class="form-control">
+              </div>
+
+
+            <!-- Column for Recipe Details -->
+
+              <div class="form-group mt-4">
+                <label for="recipeName" class="form-label">Recipe Name:</label>
+                <input type="text" id="recipeName" v-model="recipeName" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="creator" class="form-label">Creator:</label>
+                <input type="text" id="creator" v-model="creator" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="mealType" class="form-label">Meal Type:</label>
+                <select id="mealType" v-model="mealType" class="form-select">
+                  <option value="Breakfast">Breakfast</option>
+                  <option value="Brunch">Brunch</option>
+                  <option value="Lunch">Lunch</option>
+                  <option value="Dinner">Dinner</option>
+                  <option value="Supper">Supper</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="cuisineType" class="form-label">Cuisine Type:</label>
+                <select id="cuisineType" v-model="cuisineType" class="form-select">
+                  <option value="Chinese">Chinese</option>
+                  <option value="Western">Western</option>
+                  <option value="Italian">Italian</option>
+                  <option value="Japanese">Japanese</option>
+                  <option value="Korean">Korean</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="description" class="form-label">Description:</label>
+                <textarea id="description" v-model="description" class="form-control"></textarea>
+              </div>
             </div>
+
+          
+          <!-- Ingredients Form -->
+          <div class="form-group mt-4">
+            <h3>Ingredients</h3>
+            <div v-for="(ingredient, index) in ingredients" :key="index" class="ingredient-item">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="ingredientName" class="form-label">Ingredient Name:</label>
+                    <input type="text" v-model="ingredient.name" class="form-control">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="ingredientQuantity" class="form-label">Ingredient Quantity:</label>
+                    <input type="text" v-model="ingredient.quantity" class="form-control">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <button type="button" @click="removeIngredient(index)" class="btn btn-danger mt-3">Remove Ingredient</button>
+                </div>
+              </div>
+            </div>
+            <button type="button" @click="addIngredient" class="btn btn-primary mt-3">Add Ingredient</button>
           </div>
 
-          <!-- Column for Recipe Details -->
-          <div class="col-md-6">
-            <div class="form-group mt-4">
-              <label for="recipeName" class="form-label">Recipe Name:</label>
-              <input type="text" id="recipeName" v-model="recipeName" class="form-control">
-            </div>
-            <div class="form-group">
-              <label for="creator" class="form-label">Creator:</label>
-              <input type="text" id="creator" v-model="creator" class="form-control">
-            </div>
-            <div class="form-group">
-              <label for="mealType" class="form-label">Meal Type:</label>
-              <select id="mealType" v-model="mealType" class="form-select">
-                <option value="Breakfast">Breakfast</option>
-                <option value="Brunch">Brunch</option>
-                <option value="Lunch">Lunch</option>
-                <option value="Dinner">Dinner</option>
-                <option value="Supper">Supper</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="cuisineType" class="form-label">Cuisine Type:</label>
-              <select id="cuisineType" v-model="cuisineType" class="form-select">
-                <option value="Chinese">Chinese</option>
-                <option value="Western">Western</option>
-                <option value="Italian">Italian</option>
-                <option value="Japanese">Japanese</option>
-                <option value="Korean">Korean</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="description" class="form-label">Description:</label>
-              <textarea id="description" v-model="description" class="form-control"></textarea>
-            </div>
-          </div>
-        </div>
-        
-        <!-- Ingredients Form -->
-        <div class="form-group mt-4">
-          <h3>Ingredients</h3>
-          <div v-for="(ingredient, index) in ingredients" :key="index" class="ingredient-item">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="ingredientName" class="form-label">Ingredient Name:</label>
-                  <input type="text" v-model="ingredient.name" class="form-control">
+          <!-- Steps Form -->
+          <div class="form-group mt-4">
+            <h3>Steps</h3>
+            <div v-for="(step, index) in steps" :key="index" class="step-item">
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <label for="stepDescription" class="form-label">Step Description:</label>
+                    <textarea v-model="step.description" class="form-control"></textarea>
+                  </div>
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="ingredientQuantity" class="form-label">Ingredient Quantity:</label>
-                  <input type="text" v-model="ingredient.quantity" class="form-control">
+              <div class="row">
+                <div class="col">
+                  <button type="button" @click="removeStep(index)" class="btn btn-danger mt-3">Remove Step</button>
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col">
-                <button type="button" @click="removeIngredient(index)" class="btn btn-danger mt-3">Remove Ingredient</button>
-              </div>
-            </div>
+            <button type="button" @click="addStep" class="btn btn-primary mt-3">Add Step</button>
           </div>
-          <button type="button" @click="addIngredient" class="btn btn-primary mt-3">Add Ingredient</button>
-        </div>
-
-        <!-- Steps Form -->
-        <div class="form-group mt-4">
-          <h3>Steps</h3>
-          <div v-for="(step, index) in steps" :key="index" class="step-item">
-            <div class="row">
-              <div class="col">
-                <div class="form-group">
-                  <label for="stepDescription" class="form-label">Step Description:</label>
-                  <textarea v-model="step.description" class="form-control"></textarea>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <button type="button" @click="removeStep(index)" class="btn btn-danger mt-3">Remove Step</button>
-              </div>
-            </div>
-          </div>
-          <button type="button" @click="addStep" class="btn btn-primary mt-3">Add Step</button>
-        </div>
 
         <!-- Create Recipe Button -->
         <button type="button" @click="uploadImageAndCreateRecipe" class="btn btn-success mt-4">Create Recipe</button>
       </div>
+      
     </div>
   </div>
 </template>
@@ -208,10 +209,10 @@ const uploadImageAndCreateRecipe = async () => {
 };
 </script>
 
-<style scoped>
+<style>
   /* Card styles */
   .card {
-    max-width: 2000px;
+    max-width: 2200px;
     margin: 0 auto;
     padding: 20px;
     border: 1px solid #e0e0e0;
