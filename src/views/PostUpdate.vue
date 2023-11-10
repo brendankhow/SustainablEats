dc96e9856fef8880894ce85fca86aafb48684bb9
WASDuser <william.tan.2022@scis.smu.edu.sg>
2023-11-10 11:10:27 +0800

    implement loading spinner

--- src/views/PostUpdate.vue
+++ src/views/PostUpdate.vue
@@ -328,7 +328,96 @@ const handleImageUpload = (event) => {
     border: 1px solid #ccc;
     border-radius: 5px;
   }
-  
+     /* loading spinner */
+    .loading-row,
+    .loading-content{
+      height: 100%;
+    }
+
+    .loading-content{
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+
+    .loader  {
+      animation: rotate 1s infinite;  
+      height: 50px;
+      width: 50px;
+    }
+
+    .loader:before,
+    .loader:after {   
+      border-radius: 50%;
+      content: '';
+      display: block;
+      height: 20px;  
+      width: 20px;
+    }
+    .loader:before {
+      animation: ball1 1s infinite;  
+      background-color: #cb2025;
+      box-shadow: 30px 0 0 #f8b334;
+      margin-bottom: 10px;
+    }
+    .loader:after {
+      animation: ball2 1s infinite; 
+      background-color: #00a096;
+      box-shadow: 30px 0 0 #97bf0d;
+    }
+      /* Additional Styles */
+      /* Adjust styles as needed for your specific design */
+      .row {
+        margin-bottom: 20px;
+      }
+
+
+    @keyframes rotate {
+      0% { 
+        -webkit-transform: rotate(0deg) scale(0.8); 
+        -moz-transform: rotate(0deg) scale(0.8);
+      }
+      50% { 
+        -webkit-transform: rotate(360deg) scale(1.2); 
+        -moz-transform: rotate(360deg) scale(1.2);
+      }
+      100% { 
+        -webkit-transform: rotate(720deg) scale(0.8); 
+        -moz-transform: rotate(720deg) scale(0.8);
+      }
+    }
+
+    @keyframes ball1 {
+      0% {
+        box-shadow: 30px 0 0 #f8b334;
+      }
+      50% {
+        box-shadow: 0 0 0 #f8b334;
+        margin-bottom: 0;
+        -webkit-transform: translate(15px,15px);
+        -moz-transform: translate(15px, 15px);
+      }
+      100% {
+        box-shadow: 30px 0 0 #f8b334;
+        margin-bottom: 10px;
+      }
+    }
+
+    @keyframes ball2 {
+      0% {
+        box-shadow: 30px 0 0 #97bf0d;
+      }
+      50% {
+        box-shadow: 0 0 0 #97bf0d;
+        margin-top: -20px;
+        -webkit-transform: translate(15px,15px);
+        -moz-transform: translate(15px, 15px);
+      }
+      100% {
+        box-shadow: 30px 0 0 #97bf0d;
+        margin-top: 0;
+      }
+    }
   textarea {
     width: 100%;
     padding: 5px;
