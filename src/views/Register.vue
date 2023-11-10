c9d197961710a5a71092b2bf75f9d088043cdfeb
Cheryl Toh Wen Qi <wenqicheryl@gmail.com>
2023-11-10 15:27:20 +0800

    make the box top left

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -134,21 +134,16 @@ export default {
 <style scoped>
 
 .notification {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     position: fixed;
     top: 0;
     right: 0;
-    bottom: 0;
-    left: 0;
     background-color: red;
     color: white;
     padding: 10px;
     border-radius: 5px;
     margin: 10px;
     z-index: 1000;
-}
+  }
 
 *{
     margin: 0;
--- src/views/generateRecipe.vue
+++ src/views/generateRecipe.vue
@@ -519,14 +519,9 @@ const router = useRouter() // get a reference to our vue router
 
 <style>
 .notification {
-  display: flex;
-  justify-content: center;
-  align-items: center;
   position: fixed;
   top: 0;
   right: 0;
-  bottom: 0;
-  left: 0;
   background-color: red;
   color: white;
   padding: 10px;
